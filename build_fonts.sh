078fcd1fa4b0853691ddf031ff3415f1d36439ea
nathan <nathan@vertile.com>
2016-07-01 01:13:16 -0700

    move voltage to 0x06

--- build_fonts.sh
+++ build_fonts.sh
@@ -9,12 +9,12 @@ for font in "${fonts[@]}"; do
   python png2mcm.py -i images/betaflight.png -o fonts/betaflight/${font}.mcm -x 24 -y 4 -s 160
   python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0 -t 0 -c 160
   # move the symbols we need into a range outside of the logo
-  # volt
-  python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0xA9 -t 0x00 -c 1
   # throttle
   python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0xC8 -t 0x04 -c 2
   # rssi
   python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0xBA -t 0x01 -c 1
+  # volt
+  python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0xA9 -t 0x06 -c 1
   # ahi
   python copy2mcm.py -i fonts/mwosd/${font}.mcm -o fonts/betaflight/${font}.mcm -f 0xBC -t 0x27 -c 1
   # copy it into the project
